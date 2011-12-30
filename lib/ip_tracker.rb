7aaf7c3f485168bf36d8d0f08f9917fa105703e0
Sky Faber <skyf@alleninstitute.org>
2011-12-29 19:08:21 -0700

    Use the real url

--- lib/ip_tracker.rb
+++ lib/ip_tracker.rb
@@ -2,8 +2,8 @@ require "ip_tracker/version"
 
 module IpTracker
   DEFAULT_CONFIG_PATH  = '~/.ip_tracker'
-  # DEFAULT_LOCAL_TARGET = 'http://ipme.herokuapp.com'
-  DEFAULT_LOCAL_TARGET = 'http://localhost:4567'
+  DEFAULT_LOCAL_TARGET = 'http://ipme.herokuapp.com'
+  # DEFAULT_LOCAL_TARGET = 'http://localhost:4567'
   HOSTS_PATH           = '/hosts'
 end
 
